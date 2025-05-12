859fbe82398132ca2d2fac1e0091989b60a55447
mahmudurmahid <m.mahid@icloud.com>
2025-05-12 11:14:44 +0100

    debugging js for modals, styling for modals

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 document.querySelectorAll(".btn-show-more").forEach(function (button) {
   button.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevents page from jumping to the top
     const modalId = button.getAttribute("data-bs-target"); // Get the modal ID from the button
     var myModal = new bootstrap.Modal(document.querySelector(modalId));
     myModal.show();
