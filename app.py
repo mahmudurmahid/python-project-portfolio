e257c591992b22658015baf3ff6388bb5b75358b
mahmudurmahid <m.mahid@icloud.com>
2025-05-12 16:58:10 +0100

    debugged any issues with python code and index.html

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
 
-df = pd.read_csv("project_list.csv")
-print(df.head())
-
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    df = pd.read_csv("project_list.csv")
+    projects = df[[
+        "Title", "Description", "GitHub Link", "Project Type",
+        "Frontend Languages", "Backend Languages", "Frameworks",
+        "Libraries", "Database", "Status"
+    ]].to_dict(orient="records")
+    
+    return render_template("index.html", projects=projects)
 
 if __name__ == '__main__':
     app.run(debug=True)
