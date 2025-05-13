b247bd0123d22517b7ae7da22c82c550a136dce0
mahmudurmahid <m.mahid@icloud.com>
2025-05-13 10:38:47 +0100

    debug frontend and backend languages not showing on project modal

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    df = pd.read_csv("project_list.csv")
+    df = pd.read_csv("project_list.csv").fillna("")
+
     projects = df[[
         "Title", "Description", "GitHub Link", "Project Type",
         "Frontend Languages", "Backend Languages", "Frameworks",
@@ -14,6 +15,7 @@ def index():
     ]].to_dict(orient="records")
     
     return render_template("index.html", projects=projects)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
