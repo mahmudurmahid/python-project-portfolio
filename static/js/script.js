2cbb784cb0d13298096ebb53fcde43c44c0348d4
mahmudurmahid <m.mahid@icloud.com>
2025-05-13 11:19:46 +0100

    removed contact section for future development

--- static/js/script.js
+++ static/js/script.js
@@ -7,4 +7,15 @@ document.querySelectorAll(".btn-show-more").forEach(function (button) {
     myModal.show();
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Existing modal functionality
+  document.querySelectorAll(".btn-show-more").forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      const modalId = button.getAttribute("data-bs-target");
+      var myModal = new bootstrap.Modal(document.querySelector(modalId));
+      myModal.show();
+    });
+  });
 </script>
